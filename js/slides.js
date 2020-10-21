32dc36def88680c2f2cc83bf3b49925868b9a5f6
izarodrig <iza.rodrig2017@gmail.com>
2020-10-21 17:38:40 -0300

    home começo

--- js/slides.js
+++ js/slides.js
@@ -12,3 +12,14 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); 
 }
+
+/*MENU SIDE BAR*/
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "200px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
