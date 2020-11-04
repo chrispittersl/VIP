2a6c860f241fdbe055f54480c9205a44e5b15a35
izarodrig <iza.rodrig2017@gmail.com>
2020-11-04 15:33:59 -0300

    arrumei apenas o altreuniaomostra

--- rotinas/reunioes/AltReuniao_mostra.php
+++ rotinas/reunioes/AltReuniao_mostra.php
@@ -25,34 +25,49 @@
 			$res = mysqlexecuta($con,$sql);
 			$quant = (mysql_num_rows($res));
 
-			if ($quant==0) {
+			if ($quant==0) 
+			{
 				echo " Reunião não Cadastrada!! ";
-				}
-				else 	{
+			}
+			else 	
+			{
 					$row = mysql_fetch_array($res);
-				?>
-					<form name = "f1" method="POST" action="AltReuniao_altera.php">
-					Código: <?php echo $row['cod_reuniao'] ?>
-					<input type="hidden" name="cod_reuniao" value=<?php echo $row['cod_reuniao'];?>>
-					Horário da Reunião:<input type='text' name='horario_reuniao' value='<?php echo $row['horario_reuniao'];?>' required>
-					
-					Data da reunião:<input type='text' name='data_reuniao' value='<?php echo $row['data_reuniao'];?>' required>
-					
-					Descrição:<input type='text' name='descricao' value='<?php echo $row['descricao'];?>' required>
-					
-					Nome:<input type='text' name='nome' value='<?php echo $row['nome'];?>' required>
-					
-					Data de agentamento:<input type='text' name='data_agendamento' value='<?php echo $row['data_agendamento'];?>' required>
-					
-					Número de sala:<input type='text' name='num_sala' onkeypress= "return blockletras(event)" value='<?php echo $row['num_sala'];?>' required>
-					
-					Código de usuário:<input type='text' name='cod_usuario' onkeypress= "return blockletras(event)" value='<?php echo $row['cod_usuario'];?>' required>
-					
-						<input type="submit" name ="botao" value="Alterar Dados">
-					</form>
-					<?php
-				}
 		?>
+
+					<div class="container2">
+						<h1>Alteração</h1>
+						<form name = "f1" method="POST" action="AltReuniao_altera.php">
+							<label>Código de reunião</label>
+							<label id="cod"><?php echo $row['cod_reuniao'] ?></label>
+							<input type="hidden" name="cod_reuniao" value=<?php echo $row['cod_reuniao'];?>>
+
+							<label for="horario_reuniao">Horário da Reunião</label>
+							<input type='text' name='horario_reuniao' value='<?php echo $row['horario_reuniao'];?>' required>
+							
+							<label for="data_reuniao">Data da reunião</label>
+							<input type='text' name='data_reuniao' value='<?php echo $row['data_reuniao'];?>' required>
+							
+							<label for="descricao">Descrição</label>
+							<textarea name="descricao"><?php echo $row['descricao'];?> </textarea>
+							
+							<label for="nome">Nome</label>
+							<input type='text' name='nome' value='<?php echo $row['nome'];?>' required>
+							
+							<label for="data_agendamento">Data de agentamento</label>
+							<input type='text' name='data_agendamento' value='<?php echo $row['data_agendamento'];?>' required>
+							
+							<label for="num_sala">Número de sala</label>
+							<input type='text' name='num_sala' onkeypress= "return blockletras(event)" value='<?php echo $row['num_sala'];?>' required>
+							
+							<label for="cod_usuario">Código de usuário</label>
+							<input type='text' name='cod_usuario' onkeypress= "return blockletras(event)" value='<?php echo $row['cod_usuario'];?>' required>
+						
+							<input type="submit" name ="botao" value="Alterar Dados">
+						</form>
+					</div>
+				<?php
+				}
+				?>
 	<script src="../js/script.js"></script>	
 	<script src="../js/blockletras.js"></script>	
 	</body>
