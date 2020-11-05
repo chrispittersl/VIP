b2e11ac05034a0ec7b57b1938263b5a5eef42d9a
chrispittersl <christopher.silva17@etec.sp.gov.br>
2020-11-04 23:17:28 -0300

    arrumando o login

--- php/Login.php
+++ php/Login.php
@@ -11,6 +11,7 @@
 		<!--ICON-->
 		<link rel = "shortcut icon" type = "imagem/x-icon" href = "../img/logo.ico"/>
 		<!--BOOTSTRAP-->
+		<link rel="stylesheet" href="../css/bootstrap.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		
 		<!--FONTE-->
@@ -24,39 +25,47 @@
 		<div class="container">
 			<h1>Login</h1>
 
-			<form name="Login" action="../php/login.php" method="POST">
+			<form id="form-login" name="Login" action="" method="POST">
 				<label for="email"> Usuário</label>
 				<input type="text" name="user" id="user" placeholder="Digite seu usuário" maxlength="5" autocomplete="off" required>
 				<label for="password"> Senha</label>
 				<input type="password" name="senha" id="senha" placeholder="Digite sua senha" maxlength="5" autocomplete="off" onkeypress  = "return blockletras(event)" required>
-				<input type="submit" value="Login">
+				<input type="submit" value="Login" id="btn-submit">
 				<a href="#" id="visitante">Acesse como Visitante</a>
 
 			</form>
 			<?php
-				include 'config.php';
-				include 'mysqlexecuta.php';
-				$con = conectar ();
-				mysql_select_db ('vip');
-				$Usuario = $_POST['user'];
-				$Senha = $_POST ['senha'];
-				$sql="SELECT * FROM usuario where usuario like '$Usuario' && senha like '$Senha'";
-				$res = mysqlexecuta($con, $sql);
-				$quant = (mysql_num_rows($res));
 			
-				if($quant==0)
+			
+				if(isset($_POST['SubmitButton']))
 				{
-					echo "<script> alert ('Login ou Senha Inválidos !!!') </script>";
+					include 'config.php';
+					include 'mysqlexecuta.php';
+					$con = conectar ();
+					mysql_select_db ('vip');
+					$Usuario = $_POST['user'];
+					$Senha = $_POST ['senha'];
+					$sql="SELECT * FROM usuario where usuario like '$Usuario' && senha like '$Senha'";
+					$res = mysqlexecuta($con, $sql);
+					$quant = (mysql_num_rows($res));
+					if($quant==0){
+						echo "<script type='javascript'> alert('Login ou Senha Inválidos !!!'); </script>";
+					}
+					else{
+						header("location:../html/home.html");
+					}
 				}
 				else
 				{
-					header("location:../html/home.html");
+					
 				}
+				
 			?>
 
 		</div>
 
 	<!--IMPORTANDO O JAVASCRIPT-->
+	<script src="../js/jQuery.js"></script>
 	<script src="../js/script.js"></script>	
 	<script src="../js/blockletras.js"></script>		
 	</body>
