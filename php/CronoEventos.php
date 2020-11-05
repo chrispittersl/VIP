98c4f8d70553f11b90afdcfd2a6deec8e505dcf3
izarodrig <iza.rodrig2017@gmail.com>
2020-11-05 20:08:29 -0300

    CORNOgramas e o resto

--- php/CronoEventos.php
+++ php/CronoEventos.php
@@ -1,28 +1,63 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
         <title>Cronograma de TCCs</title>
-        <style>
-            table{
-                border: 1px solid;
-            }
-        </style>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--CSS-->
+        <link rel="stylesheet" href="../css/pag.css">
+        <link rel="stylesheet" href="../css/cronogramas.css">
+        <!--ICONES BOOTSTRAP-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link rel = "stylesheet" href = "https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity = "sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin = "anonymous">
+        <!--ICON PAGINA-->
+        <link rel = "shortcut icon" type = "imagem/x-icon" href = "../img/logo.ico"/>
+        <!--FONTE GOOGLE USADA-->
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow&display=swap" rel="stylesheet">
+        <!--IMPORTANDO JQUERY-->
+        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    
     </head>
+
     <body>
+        <!--MENU E NAVEGAÇÃO-->
+      <header class="cabecalho">
+          <a href="home.html"><h1 class="logo">Virtual Increased Plan</h1></a>
+          <div class="btn">
+            <span class="fas fa-bars"></span>
+          </div>
+          <nav class="sidebar">
+            <div id="mySidebar" class="sidebar">
+              <ul>
+                <li class="margin"><a href="#">Home</a></li>
+                <li>
+                  <button class="dropdown-btn">Cronogramas
+                    <i class="fa fa-caret-down"></i>
+                  </button>
+                  <ul class="dropdown-container">
+                    <li><a href="#">TCCs</a></li>
+                    <li><a href="#">Reuniões de pais</a></li> 
+                  </ul>
+                </li>
+                <li><a href="homeadm.html">Administrador</a></li>
+              </ul>
+            </div>
+          </nav>
+      </header>  
+
         <h1>Cronograma de TCCs</h1>
         <table>
-            <caption>Trabalhos de Conclusão de Curso</caption>
-            <thead>
+            
+            <!-- <!-- <thead> -->
                 <tr>
                     <th>TCC</th>
                     <th>Descrição</th>
                     <th>Horário</th>
                     <th>Data</th>
                 </tr>
-            </thead>
-
-            <tbody>
+            <!-- </thead> -->
+        
+            <!-- <tbody> -->
                 <?php 
                 
                     include 'config.php';
@@ -35,26 +70,45 @@
                     $consulta = mysqlexecuta($con,$sql);
 
                     //if($rows > 0){ 
-                        while($row = mysql_fetch_assoc($consulta)){
-                            echo '
+                        while($row = mysql_fetch_assoc($consulta))
+                        {
+                            ?>
                                 <tr>
-                                    <td>'.$row['nome'].'</td>
-                                    <td>'.$row['descricao'].'</td>
-                                    <td>'.$row['horario_tcc'].'</td>
-                                    <td>'.$row['data_tcc'].'</td>
-                                </tr>';
+                                
+                                    <td><?php echo utf8_encode($row['nome']);?></td>
+                                    <td><?php echo utf8_encode($row['descricao']);?></td>
+                                    <td><?php echo utf8_encode($row['horario_tcc']);?></td>
+                                    <td><?php echo utf8_encode($row['data_tcc']);?></td>
+                                </tr>
+                           <?php
                                     
                         }
-                    //}
-                    /*else{
-                        echo '
-                            <tr>
-                                <td>Cronogramas indisponível!</td>
-                            </tr>
-                        ';
-                    }*/
-                ?>
-            </tbody>
+            
+                        ?>
+            <!-- </tbody> -->
         </table>
+
+        <!--SCRIPT PARA NAVBAR COLLAPSE-->
+      <script>
+            $('.btn').click(function(){
+              $(this).toggleClass("click");
+              $('.sidebar').toggleClass("show");
+            });
+            
+            var dropdown = document.getElementsByClassName("dropdown-btn");
+            var i;
+
+            for (i = 0; i < dropdown.length; i++) {
+              dropdown[i].addEventListener("click", function() {
+              this.classList.toggle("active");
+              var dropdownContent = this.nextElementSibling;
+              if (dropdownContent.style.display === "block") {
+              dropdownContent.style.display = "none";
+              } else {
+              dropdownContent.style.display = "block";
+              }
+              });
+            }
+      </script>
     </body>
 </html>
