24673e0072abd5eab5650c30ae708adae7750a89
Christopher Pitter <christopher.silva17@etec.sp.gov.br>
2020-11-17 14:42:20 -0300

    .js do login

---
+++ js/login.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('#errorLogin').hide();
+    $("#form-login").on("submit",function(e){
+        e.preventDefault();
+
+        var user = $('#user').val();
+        var password = $('#password').val();
+        
+        $.ajax({
+            url: 'http://localhost/VIP/php/validarLogin.php',
+            method: 'POST',
+            data: {user: user, password: password},
+            dataType: 'json'
+        }).done(function(result){
+            $('#user').val('');
+            $('#password').val('');
+
+            if(result == "válido"){
+                window.location.href = "home.html";
+            }
+            else{
+                $('#errorLogin').show();
+            }
+        })
+    })
+})
