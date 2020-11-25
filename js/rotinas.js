175b9c1eded160e98c57a11795febb628c044ce2
Christopher Pitter <christophsilvaa@gmail.com>
2020-11-25 14:28:49 -0300

    rotinas incompletas

--- js/rotinas.js
+++ js/rotinas.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    // form Alterar Horarios
     $("#form-altera-horarios").on("submit",function(e){
         e.preventDefault();
         var cod_horario = $('#cod_horario').val();
@@ -11,7 +13,7 @@ $(document).ready(function(){
         var professor = $('#professor').val();
         
         $.ajax({
-            url: 'http://localhost/VIP/php/altHorarios_altera.php',
+            url: 'http://localhost/VIP/rotinas/horarios/altHorarios_altera.php',
             method: 'POST',
             data: {cod_usuario: cod_usuario, cod_turma: cod_turma, 
                 horainicio: horainicio, horafim: horafim,
@@ -29,66 +31,7 @@ $(document).ready(function(){
             }
         })
     })
-    $("#form-altera-reunioes").on("submit",function(e){
-        e.preventDefault();
-        var cod_reuniao = $('#cod_reuniao').val();
-        var horario_reuniao = $('#horario_reuniao').val();
-        var data_reuniao = $('#data_reuniao').val();
-        var descricao = $('#descricao').val();
-        var nome = $('#nome').val();
-        var data_agendamento = $('#data_agendamento').val();
-        var num_sala = $('#num_sala').val();
-        var cod_usuario = $('#cod_usuario').val();
-        
-        $.ajax({
-            url: 'http://localhost/VIP/php/AltReuniao_altera.php',
-            method: 'POST',
-            data: {cod_reuniao: cod_reuniao, horario_reuniao: horario_reuniao, 
-                data_reuniao: data_reuniao, descricao: descricao,
-                nome: nome, data_agendamento: data_agendamento,
-                num_sala: num_sala, cod_usuario: cod_usuario},
-            dataType: 'json'
-        }).done(function(result){
-            if(result == "0"){
-                alert("Registro alterado com sucesso!");
-                window.location.href = "AltReuniao.html";
-            }
-            else{
-                alert("Falha ao alterar o registro, favor contactar o administrador.");
-                window.location.href = "../../teste/homeadm.html";
-            }
-        })
-    })
-    $("#form-altera-tcc").on("submit",function(e){
-        e.preventDefault();
-        var cod_tcc = $('#cod_tcc').val();
-        var horario_tcc = $('#horario_tcc').val();
-        var data_tcc = $('#data_tcc').val();
-        var descricao = $('#descricao').val();
-        var nome = $('#nome').val();
-        var data_agendamento = $('#data_agendamento').val();
-        var num_sala = $('#num_sala').val();
-        var cod_usuario = $('#cod_usuario').val();
-        
-        $.ajax({
-            url: 'http://localhost/VIP/php/AltTcc_altera.php',
-            method: 'POST',
-            data: {cod_tcc: cod_tcc, horario_tcc: horario_tcc, 
-                data_tcc: data_tcc, descricao: descricao,
-                nome: nome, data_agendamento: data_agendamento,
-                num_sala: num_sala, cod_usuario: cod_usuario},
-            dataType: 'json'
-        }).done(function(result){
-            if(result == "0"){
-                alert("Registro alterado com sucesso!");
-                window.location.href = "AltTcc.html";
-            }
-            else{
-                alert("Falha ao alterar o registro, favor contactar o administrador.");
-                window.location.href = "../../teste/homeadm.html";
-            }
-        })
-    })
+    // form Cadastrar Horarios
     $("#form-cadastrar-horarios").on("submit",function(e){
         e.preventDefault();
         var cod_usuario = $('#cod_usuario').val();
@@ -100,7 +43,7 @@ $(document).ready(function(){
         var professor = $('#professor').val();
         
         $.ajax({
-            url: 'http://localhost/VIP/php/cadHorarios.php',
+            url: 'http://localhost/VIP/rotinas/horarios/cadHorarios.php',
             method: 'POST',
             data: {cod_usuario: cod_usuario, cod_turma: cod_turma, 
                 horainicio: horainicio, horafim: horafim,
@@ -118,6 +61,39 @@ $(document).ready(function(){
             }
         })
     })
+
+    // form Alterar Reunioes
+    $("#form-altera-reunioes").on("submit",function(e){
+        e.preventDefault();
+        var cod_reuniao = $('#cod_reuniao').val();
+        var horario_reuniao = $('#horario_reuniao').val();
+        var data_reuniao = $('#data_reuniao').val();
+        var descricao = $('#descricao').val();
+        var nome = $('#nome').val();
+        var data_agendamento = $('#data_agendamento').val();
+        var num_sala = $('#num_sala').val();
+        var cod_usuario = $('#cod_usuario').val();
+        
+        $.ajax({
+            url: 'http://localhost/VIP/rotinas/reunioes/AltReuniao_altera.php',
+            method: 'POST',
+            data: {cod_reuniao: cod_reuniao, horario_reuniao: horario_reuniao, 
+                data_reuniao: data_reuniao, descricao: descricao,
+                nome: nome, data_agendamento: data_agendamento,
+                num_sala: num_sala, cod_usuario: cod_usuario},
+            dataType: 'json'
+        }).done(function(result){
+            if(result == "0"){
+                alert("Registro alterado com sucesso!");
+                window.location.href = "AltReuniao.html";
+            }
+            else{
+                alert("Falha ao alterar o registro, favor contactar o administrador.");
+                window.location.href = "../../teste/homeadm.html";
+            }
+        })
+    })
+    // form Cadastrar Reunioes
     $("#form-cadastrar-reunioes").on("submit",function(e){
         e.preventDefault();
         var cod_usuario = $('#cod_usuario').val();
@@ -129,7 +105,72 @@ $(document).ready(function(){
         var professor = $('#professor').val();
         
         $.ajax({
-            url: 'http://localhost/VIP/php/cadHorarios.php',
+            url: 'http://localhost/VIP/rotinas/horarios/CadReuniao.php',
+            method: 'POST',
+            data: {cod_usuario: cod_usuario, cod_turma: cod_turma, 
+                horainicio: horainicio, horafim: horafim,
+                diadasemana: diadasemana, materia: materia,
+                professor: professor},
+            dataType: 'json'
+        }).done(function(result){
+            if(result == "0"){
+                alert("Registro cadastrado com sucesso!");
+                window.location.href = "cadHorarios.html";
+            }
+            else{
+                alert("Falha ao cadastrar o registro, favor contactar o administrador.");
+                window.location.href = "../../teste/homeadm.html";
+            }
+        })
+    })
+
+    // form Alterar Tcc
+    $("#form-altera-tcc").on("submit",function(e){
+        e.preventDefault();
+        var cod_tcc = $('#cod_tcc').val();
+        var horario_tcc = $('#horario_tcc').val();
+        var data_tcc = $('#data_tcc').val();
+        var descricao = $('#descricao').val();
+        var nome = $('#nome').val();
+        var data_agendamento = $('#data_agendamento').val();
+        var num_sala = $('#num_sala').val();
+        var cod_usuario = $('#cod_usuario').val();
+        
+        $.ajax({
+            url: 'http://localhost/VIP/rotinas/tcc/AltTcc_altera.php',
+            method: 'POST',
+            data: {cod_tcc: cod_tcc, horario_tcc: horario_tcc, 
+                data_tcc: data_tcc, descricao: descricao,
+                nome: nome, data_agendamento: data_agendamento,
+                num_sala: num_sala, cod_usuario: cod_usuario},
+            dataType: 'json'
+        }).done(function(result){
+            if(result == "0"){
+                alert("Registro alterado com sucesso!");
+                window.location.href = "AltTcc.html";
+            }
+            else{
+                alert("Falha ao alterar o registro, favor contactar o administrador.");
+                window.location.href = "../../teste/homeadm.html";
+            }
+        })
+    })
+
+    
+
+    
+    $("#form-cadastrar-tcc").on("submit",function(e){
+        e.preventDefault();
+        var cod_usuario = $('#cod_usuario').val();
+        var cod_turma = $('#cod_turma').val();
+        var horainicio = $('#horainicio').val();
+        var horafim = $('#horafim').val();
+        var diadasemana = $('#diadasemana').val();
+        var materia = $('#materia').val();
+        var professor = $('#professor').val();
+        
+        $.ajax({
+            url: 'http://localhost/VIP/rotinas/tcc/CadTcc.php',
             method: 'POST',
             data: {cod_usuario: cod_usuario, cod_turma: cod_turma, 
                 horainicio: horainicio, horafim: horafim,
