ae126966cebf1a92573542a1002503e0d35d6eec
izarodrig <iza.rodrig2017@gmail.com>
2020-11-26 20:54:21 -0300

    mapa completo ta pronto porra

--- php/mapa.php
+++ php/mapa.php
@@ -21,59 +21,65 @@
             <div class="btn">
                   <span class="fas fa-bars fa-2x"></span>
             </div>
+            <div class="btns">
+                <a href="#"><button class="btn-reset margin-zero">RESETAR</button></a>
+                <a href="#"><button  class="btn-reset">SAIR</button></a>
+            </div>
+
             <nav class="legenda">
                 <div id="mySidebar" class="sidebar">
-                <ul class="items-legenda">
-                    <li >
-                        <button class="dropdown-btn margin-top">BLOCO ADMIN <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
-                        <ul class="dropdown-container">
-                            <li><button class="rota" id="btn-anexo">Anexo</button></li>
-                            <li><button class="rota" id="btn-refeitorio">Refeitório</button></li>
-                            <li><button class="rota" id="btn-miniauditorio">Miniauditório</button></li>
-                            <li><button class="rota" id="btn-secretaria">Secretaria</button></li>
-                            <li><button class="rota" id="btn-banheiro_patio_fem">Banheiro Pátio Fem</button></li>
-                            <li><button class="rota" id="btn-banheiro_patio_masc">Banheiro Pátio Masc</button></li>
-                            <li><button class="rota" id="btn-biblioteca">Biblioteca</button></li>
-                            <li><button class="rota margin-bottom" id="btn-enfermaria">Enfermaria</button></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <button class="dropdown-btn">BLOCO A <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
-                        <ul class="dropdown-container">
-                            <li><button class="rota" id="btn-sala1">Sala 1</button></li>
-                            <li><button class="rota" id="btn-sala2">Sala 2</button></li>
-                            <li><button class="rota" id="btn-sala3">Sala 3</button></li>
-                            <li><button class="rota" id="btn-sala4">Sala 4</button></li>
-                            <li><button class="rota" id="btn-sala5">Sala 5</button></li>
-                            <li><button class="rota" id="btn-sala6">Sala 6</button></li>
-                            <li><button class="rota" id="btn-sala7">Sala 7</button></li>
-                            <li><button class="rota margin-bottom" id="btn-sala8">Sala 8</button></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <button class="dropdown-btn">BLOCO B <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
-                        <ul class="dropdown-container">
-                            <li><button class="rota" id="btn-sala9">Sala 9</button></li>
-                            <li><button class="rota" id="btn-sala10">Sala 10</button></li>
-                            <li><button class="rota" id="btn-sala11">Sala 11</button></li>
-                            <li><button class="rota" id="btn-sala12">Sala 12</button></li>
-                            <li><button class="rota" id="btn-sala13">Sala 13</button></li>
-                            <li><button class="rota" id="btn-sala14">Sala 14</button></li>
-                            <li><button class="rota" id="btn-sala15">Sala 15</button></li>
-                            <li><button class="rota margin-bottom" id="btn-sala16">Sala 16</button></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <button class="dropdown-btn">OUTROS <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
-                        <ul class="dropdown-container">
-                            <li><button class="rota" id="btn-portaria">Portaria</button></li>
-                            <li><button class="rota" id="btn-auditorio">Auditório</button></li>
-                            <li><button class="rota" id="btn-cantina">Cantina</button></li>
-                            <li><button class="rota" id="btn-quadra_coberta">Quadra Coberta</button></li>
-                            <li><button class="rota margin-bottom" id="btn-quadra_descoberta">Quadra Descoberta</button></li>
-                        </ul>
-                    </li>
-                        
+                    <ul class="items-legenda">
+                        <li >
+                            <button class="dropdown-btn margin-top">BLOCO ADMIN <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
+                            <ul class="dropdown-container">
+                                <li><button class="rota" id="btn-anexo">Anexo</button></li>
+                                <li><button class="rota" id="btn-refeitorio">Refeitório</button></li>
+                                <li><button class="rota" id="btn-miniauditorio">Miniauditório</button></li>
+                                <li><button class="rota" id="btn-secretaria">Secretaria</button></li>
+                                <li><button class="rota" id="btn-banheiro_patio_fem">Banheiro Pátio Fem</button></li>
+                                <li><button class="rota" id="btn-banheiro_patio_masc">Banheiro Pátio Masc</button></li>
+                                <li><button class="rota" id="btn-biblioteca">Biblioteca</button></li>
+                                <li><button class="rota margin-bottom" id="btn-enfermaria">Enfermaria</button></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button class="dropdown-btn">BLOCO A <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
+                            <ul class="dropdown-container">
+                                <li><button class="rota" id="btn-sala1">Sala 1</button></li>
+                                <li><button class="rota" id="btn-sala2">Sala 2</button></li>
+                                <li><button class="rota" id="btn-sala3">Sala 3</button></li>
+                                <li><button class="rota" id="btn-sala4">Sala 4</button></li>
+                                <li><button class="rota" id="btn-sala5">Sala 5</button></li>
+                                <li><button class="rota" id="btn-sala6">Sala 6</button></li>
+                                <li><button class="rota" id="btn-sala7">Sala 7</button></li>
+                                <li><button class="rota margin-bottom" id="btn-sala8">Sala 8</button></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button class="dropdown-btn">BLOCO B <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
+                            <ul class="dropdown-container">
+                                <li><button class="rota" id="btn-sala9">Sala 9</button></li>
+                                <li><button class="rota" id="btn-sala10">Sala 10</button></li>
+                                <li><button class="rota" id="btn-sala11">Sala 11</button></li>
+                                <li><button class="rota" id="btn-sala12">Sala 12</button></li>
+                                <li><button class="rota" id="btn-sala13">Sala 13</button></li>
+                                <li><button class="rota" id="btn-sala14">Sala 14</button></li>
+                                <li><button class="rota" id="btn-sala15">Sala 15</button></li>
+                                <li><button class="rota margin-bottom" id="btn-sala16">Sala 16</button></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <button class="dropdown-btn">OUTROS <i class="fa fa-chevron-circle-down" aria-hidden="true"></i></button>
+                            <ul class="dropdown-container">
+                                <li><button class="rota" id="btn-portaria">Portaria</button></li>
+                                <li><button class="rota" id="btn-auditorio">Auditório</button></li>
+                                <li><button class="rota" id="btn-cantina">Cantina</button></li>
+                                <li><button class="rota" id="btn-quadra_coberta">Quadra Coberta</button></li>
+                                <li><button class="rota margin-bottom" id="btn-quadra_descoberta">Quadra Descoberta</button></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>   
             </nav>
         </header>
 
