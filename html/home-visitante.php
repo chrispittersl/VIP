828ae36f79800860548fc17f8ab5c29ab612217c
Christopher Pitter <christophsilvaa@gmail.com>
2020-11-28 18:29:25 -0300

    Alterei o titulo da pagina

--- html/home-visitante.php
+++ html/home-visitante.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Home</title>
+        <title>Home Visitante</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!--CSS-->
